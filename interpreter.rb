440bdebb1282c31726574e9cb04493d97c7f12c7
Alexey Sorokin <alsor.net@gmail.com>
2017-11-13 00:31:58 +0300

    numbers and symbols

--- interpreter.rb
+++ interpreter.rb
@@ -1,5 +1,28 @@
-def scheme(source)
-  reader(source)
+def lisp(source)
+  tokens = tokenize(source)
+  reader(tokens).each do |exp|
+    eval(exp, {})
+  end
+end
+
+class Token
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def number?
+    name =~ /^\d/
+  end
+
+  def symbol?
+    name =~ /^\D/
+  end
+
+  def inspect
+    "\"#{name}\""
+  end
 end
 
 OPEN_PARAM = '('
@@ -12,7 +35,7 @@ def tokenize(source)
   source.split("").each do |character|
     if character =~ /\s/ || character == ','
       next if token == ''
-      tokens << token
+      tokens << Token.new(token)
       token = ''
     else
       if LEXEMS.include?(character)
@@ -24,13 +47,14 @@ def tokenize(source)
       end
     end
   end
+
+  tokens << Token.new(source) if tokens.empty?
   tokens
 end
 
-def reader(source)
+def reader(tokens)
   tree = []
   stack = []
-  tokens = tokenize(source)
   puts "tokens: #{tokens.inspect}"
   tokens.each do |token|
     if token == OPEN_PARAM
@@ -40,13 +64,25 @@ def reader(source)
     elsif token == CLOSE_PARAM
       stack.pop
     else
-      stack.last << token
+      (stack.last || tree) << token
     end
   end
+
+  # ['do', tree].tap { |tree| puts "tree: #{tree.inspect}" }
   puts "tree: #{tree.inspect}"
   tree
 end
 
-# reader("(+ abc,   123 (abc a12 123.0))")
-# reader("(a b (c d (1 2 3) x) e f)")
-reader("(a b)\n(c d)")
+def eval(exp, env)
+  if exp.number?
+    exp
+  elsif exp.symbol?
+    env.fetch(exp.name) { fail "Undefined variable #{exp.inspect}" }
+  end
+end
+
+# lisp("(+ abc,   123 (abc a12 123.0))")
+# lisp("(a b (c d (1 2 3) x) e f)")
+# lisp("(a b)\n(c d)")
+puts lisp('5').inspect
+puts lisp('a').inspect
